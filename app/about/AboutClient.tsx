80dcc979068366ee62b317c304a0cefd1d941655
Mohamad Khademy <106902964+MhmdKh7@users.noreply.github.com>
2026-09-02 14:59:05 +0000

    fix: app/about/AboutClient.tsx

--- app/about/AboutClient.tsx
+++ app/about/AboutClient.tsx
@@ -2,8 +2,8 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import Header from "@/components/header";
-import Footer from "@/components/footer";
+import { Header } from "@/components/header";
+import { Footer } from "@/components/footer";
 import {
     ChevronLeft,
     Award,
