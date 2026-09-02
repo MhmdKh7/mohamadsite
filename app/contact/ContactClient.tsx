c8db78f168b89a3d78785ae877651ce66639f9de
Mohamad Khademy <106902964+MhmdKh7@users.noreply.github.com>
2026-09-02 14:59:07 +0000

    fix: app/contact/ContactClient.tsx

--- app/contact/ContactClient.tsx
+++ app/contact/ContactClient.tsx
@@ -2,8 +2,8 @@
 
 import { useState } from "react";
 import Link from "next/link";
-import Header from "@/components/header";
-import Footer from "@/components/footer";
+import { Header } from "@/components/header";
+import { Footer } from "@/components/footer";
 import {
     ChevronLeft,
     Phone,
