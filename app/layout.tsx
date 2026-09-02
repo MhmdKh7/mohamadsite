36d9584d97f5ee68a8b1624052a71787407f3314
Mohamad Khademy <106902964+MhmdKh7@users.noreply.github.com>
2026-09-02 14:38:04 +0000

    seo: update app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -109,9 +109,9 @@ export const metadata: Metadata = {
   },
 
   icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon.ico",
-    apple: "/apple-touch-icon.png",
+    icon: "/images/logo.png",
+    shortcut: "/images/logo.png",
+    apple: "/images/logo.png",
   },
 
   category: "Industrial Equipment",
