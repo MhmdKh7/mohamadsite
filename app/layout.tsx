c12b927723728fc2acdc7d63d0511eb0fe7d7322
Mohamad Khademy <106902964+MhmdKh7@users.noreply.github.com>
2026-09-22 12:02:42 +0330

    feat: add Yektanet analytics script to root layout head

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata, Viewport } from 'next'
 import { Vazirmatn } from 'next/font/google'
 import './globals.css'
 import { Toaster } from '@/components/ui/sonner'
+import Script from 'next/script'
 
 const vazirmatn = Vazirmatn({
   subsets: ['arabic', 'latin'],
@@ -131,6 +132,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fa" dir="rtl" className="bg-background">
+      <head>
+
+        <Script id="yektanet-analytics" strategy="afterInteractive">{`!function (t, e, n) {
+        t.yektanetAnalyticsObject = n, t[n] = t[n] || function () {
+            t[n].q.push(arguments)
+        }, t[n].q = t[n].q || [];
+        var a = new Date, r = a.getFullYear().toString() + "0" + a.getMonth() + "0" + a.getDate() + "0" + a.getHours(),
+            c = e.getElementsByTagName("script")[0], s = e.createElement("script");
+        s.id = "ua-script-MUCDSUrd"; s.dataset.analyticsobject = n;
+        s.async = 1; s.type = "text/javascript";
+        s.src = "https://cdn.yektanet.com/rg_woebegone/scripts_v3/MUCDSUrd/rg.complete.js?v=" + r, c.parentNode.insertBefore(s, c)
+    }(window, document, "yektanet");`}</Script>
+      </head>
       <body className={`${vazirmatn.className} font-sans antialiased`}>
         <StructuredData />
         {children}
